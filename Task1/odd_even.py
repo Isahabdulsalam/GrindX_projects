4c43ad7227f25a292ea25bcbe20966a5867d6ba3
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2025-01-21 13:01:28 +0100

    Task 1

---
+++ Task1/odd_even.py
@@ -0,0 +1,10 @@
+# Write a program to check if a number is even or odd.
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        print(f'{number} is even')
+    else:
+        print(f'{number} is odd')
+
+
+odd_or_even(5)
